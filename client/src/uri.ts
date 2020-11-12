60343a51153d24bb61a3a914e92dc96e92637166
nosknut <nosknut@gmail.com>
2020-11-12 20:51:00 +0100

    Fixes deployment issues
    Adds typescript and types to server dependencies
    Ajusts client endpoint path
    Adds method for detecting prod env
    Adds own api path for dev and prod
    Adds redirect and subpath handling for client

--- client/src/uri.ts
+++ client/src/uri.ts
@@ -1,1 +1,10 @@
-export const API_URI = 'http://localhost:80'
+import React from 'react';
+
+/**
+ * React elements have the property _self defined, whereas in production mode that property is not defined
+ */
+function isProd() {
+    return !('_self' in React.createElement('div'));
+}
+
+export const API_URI = isProd() ? String(window.location.origin) : 'http://localhost';
